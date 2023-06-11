2170bb3b9adaadf9b8fb02700fa877c64ef5b6fe
scaferuz <endrafruz@gmail.com>
2023-06-11 18:56:02 +0700

    adding bag of words function

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,6 @@
 from nltk.stem.porter import PorterStemmer
 import nltk
+import numpy as np
 # nltk.download('punkt')
 stemmer = PorterStemmer()
 
@@ -13,4 +14,16 @@ def stem(word):
 
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    """
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog =   [   0,       1,   0,     1,     0,       0,      0]
+    """
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+    return bag
